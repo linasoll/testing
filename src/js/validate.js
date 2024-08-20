9c1bcf8580504c2c98b6502bcffc0c2e606a8742
lina sol <soldatenko_angelina@mail.ru>
2024-08-20 20:02:17 +0400

    try

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,13 +1,12 @@
 export function validateCard(cardNumber) {
-    const cardNumberString = cardNumber.toString();
     let sum = 0;
 
-    if (cardNumberString.length !== 16) {
+    if (cardNumber.length !== 16) {
         alert("Неверная длина номера");
     }
 
-    for (let i=0; i<cardNumberString.length; i++) {
-        let digit = Number(cardNumberString[i]);
+    for (let i=0; i<cardNumber.length; i++) {
+        let digit = Number(cardNumber[i]);
         if (i % 2 === 0) {
             digit *=2;
             if (digit > 9) {
